88a2d8aa7bdf7017d31067360adee5e9f99cbad8
Rika Sumitomo <muu_and_sin@yahoo.co.jp>
2019-04-10 20:30:01 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by Rika Sumitomo on 2019/04/10.
+//  Copyright © 2019 Rika Sumitomo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
