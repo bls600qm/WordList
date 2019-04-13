4aa29f799048dba5fb6b5fdbd4f1ea6df34a6633
Rika Sumitomo <muu_and_sin@yahoo.co.jp>
2019-04-13 15:31:53 +0900

    QuestionViewController.swiftにシャッフル，次へボタンを押した時の処理を書いた

--- WordList/QuestionViewController.swift
+++ WordList/QuestionViewController.swift
@@ -10,21 +10,53 @@ import UIKit
 
 class QuestionViewController: UIViewController {
 
+    @IBOutlet var nextButton: UIButton!
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    var isAnswerd: Bool = false
+    var wordArray: [Dictionary<String, String>] = []
+    var shuffledWordArray: [Dictionary<String, String>] = []
+    var nowNumber: Int = 0
+    
+    let saveData = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        answerLabel.text = ""
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) { //Viewが現れた時に呼ばれる
+        super.viewWillAppear(animated)
+        wordArray = saveData.array(forKey: "WORD") as! [Dictionary<String, String>]
+        shuffle()
+        questionLabel.text = shuffledWordArray[nowNumber]["english"]
     }
-    */
-
+    
+    func shuffle() {
+        while wordArray.count > 0 {
+            let index = Int(arc4random()) % wordArray.count
+            shuffledWordArray.append(wordArray[index])
+            wordArray.remove(at: index)
+        }
+    }
+    
+    @IBAction func nextButtonTapped() {
+        if isAnswerd {
+            nowNumber += 1
+            answerLabel.text = ""
+            
+            if nowNumber < shuffledWordArray.count {
+                questionLabel.text = shuffledWordArray[nowNumber]["english"]
+                isAnswerd = false
+                nextButton.setTitle("答えを表示", for: .normal)
+            } else {
+                self.performSegue(withIdentifier: "toFinishView", sender: nil)
+            }
+        } else {
+            answerLabel.text = shuffledWordArray[nowNumber]["japanese"]
+            isAnswerd = true
+            nextButton.setTitle("次へ", for: .normal)
+        }
+    }
 }
--- WordList/ViewController.swift
+++ WordList/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func back(segue: UIStoryboardSegue) {
+        //FinishViewControllerまでいったときにスタートに戻ってくるためのメソッド
+    }
+    @IBAction func startButtonTapped(){
+        let saveData = UserDefaults.standard
+        
+        if let wordArray = saveData.array(forKey: "WORD") {
+            if wordArray.count > 0 {
+                self.performSegue(withIdentifier: "toQuestionView", sender: nil)
+            }
+        }
+        let alert:UIAlertController = UIAlertController (
+            title: "単語",
+            message: "まずは単語一覧をタップしてください",
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil
+        ))
+        self.present(alert, animated: true, completion: nil)
+    
+    
+    }
 }
 
